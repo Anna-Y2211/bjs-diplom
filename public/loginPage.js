d55f7754072ffe6b0becbebdc05ebedd3fd39afe
AnnaYou <anna2211985@ya.ru>
2024-11-12 21:11:55 +0300

    внесены правки

--- public/loginPage.js
+++ public/loginPage.js
@@ -2,21 +2,23 @@
 
 const userForm = new UserForm();
 userForm.loginFormCallback = (data) => {
- ApiConnector.login(data, callback) => {
-   if(callback.succes) {
-     location.reload();
-} else {
-   throw new Error («Ошибка! Неверный логин или пароль»);
-   }
- }
+  ApiConnector.login(data, resronse) => {
+     if(data.success) {
+      location.reload();
+     } else {
+      userForm.setLoginErrorMessage(response.error);
+      alert('Ошибка! Неверный логин или пароль');
+     }
+  }
 }
 
-userForm.registerFormCallback  = (data) => {
-  ApiConnector.login (data, callback) => {
-      if(callback.succes) {
-     location.reload();
-} else {
-   throw new Error («Ошибка! Такой логин уже занят»);
-    }
+userForm.registerFormCallback = (data) => {
+  ApiConnector.login(data, response) => {
+     if(data.success) {
+      location.reload();
+     } else {
+      userForm.setRegisterErrorVessage(response.error);
+      alert('Ошибка! Такой логин уже занят');
+     }
   }
 }
